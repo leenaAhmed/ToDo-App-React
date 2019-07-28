f539d2a568f2c245ccf95c7bed7bc80c986e8840
leenaAhmed <leena3792@gmail.com>
2019-07-28 23:47:24 +0200

    frist commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import Todos from './ToDo' ;
+import AddTodo from './addtodo' ;
+
+class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content: ''},
+     ]
+  }
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    });
+    this.setState({
+      todos
+    });
+  }
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos
+    });
+  }
+  render() {
+    return (
+      <div className="todo-app container">
+        <h1 className="todo-heading">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo} />
+      </div>
+    );
+  }
+}
+ export default App;
